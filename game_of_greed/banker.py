280ed26944d60c882d2678da610f896f56386a26
Kevin Henry <kevinhenry@mac.com>
2021-06-22 21:13:18 -0700

    Initial Commit

---
+++ game_of_greed/banker.py
@@ -0,0 +1,13 @@
+class Banker:
+
+    def __init__ (self):
+        pass
+
+    def shelf(self):
+        pass
+
+    def bank(self):
+        pass
+
+    def clear_shelf(self):
+        pass
---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,12 @@
+class GameLogic:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def calculate_score():
+        pass
+
+    @staticmethod
+    def roll_dice():
+        pass

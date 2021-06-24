7738a8f955c4f7b117e17848495d942e83e1ae60
Marie <marie.marcos25@gmail.com>
2021-06-23 19:17:12 -0700

    initialize

--- game_of_greed/banker.py
+++ game_of_greed/banker.py
@@ -1,13 +1,29 @@
 class Banker:
 
-    def __init__ (self):
-        pass
+    def __init__ (self, balance=0, shelved=0):
+        self.balance = balance
+        self.shelved = shelved
 
-    def shelf(self):
+    def shelf(self, points):
+        """
+        INPUT >> Integer - amount of points to add to shelf
+        OUTPUT >> No Output - adding points to shelved
+        """
         pass
 
     def bank(self):
+        """
+        INPUT >> no input
+        OUTPUT >> Integer - the amount from the shelf
+            - adding the shelved amount to balance
+            - reset shelved to zero
+        """
         pass
 
     def clear_shelf(self):
+        """
+        INPUT >> NONE
+        OUTPUT >> NONE
+            - reset the shelved to zero
+        """
         pass
--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -4,9 +4,21 @@ class GameLogic:
         pass
 
     @staticmethod
-    def calculate_score():
+    def calculate_score(tuple):
+        """
+        INPUTS >>  Tuple - a collection of numbers from 1-6. Length can be 1-6
+        OUTPUT >>  Integer - score based on the collection of dice
+        
+        """
         pass
 
     @staticmethod
-    def roll_dice():
+    def roll_dice(number):
+        """
+        INPUT >> Integer - a number between 1 and 6
+        OUTPUT >> Tuple - with length of input with numbers between 1 and 6
+        """
         pass
+
+
+        

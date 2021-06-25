bf8a75bbab4d1eb5eebe1863d208a92e5f2b0719
Davee Sok <daveesok@gmail.com>
2021-06-24 20:12:39 -0500

    edited calculate score, all test pass

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -2,16 +2,10 @@ import random
 from collections import Counter
 
 DIE_POINTS = {1: 100, 5: 50}
-TRIPLET_POINTS = {1: 1000 - 3 * DIE_POINTS[1],
-                  2: 200,
-                  3: 300,
-                  4: 400,
-                  5: 500 - 3 * DIE_POINTS[5],
-                  6: 600}
+TRIPLET_POINTS = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
 
 
 class GameLogic:
-
     def __init__(self):
         pass
 
@@ -20,38 +14,34 @@ class GameLogic:
         """
         INPUTS >>  Tuple - a collection of numbers from 1-6. Length can be 1-6
         OUTPUT >>  Integer - score based on the collection of dice
-        
+
         """
         count = Counter(tuple)
 
+        # Straights
         if len(count) == 6:
             return 1500
 
-        if len(count) == 1 and len(tuple) == 6:
-            if tuple[0] == 1:
-                return 4000
-
-        for property in count:
-            print("this is from line 42:", property, count[property])
-            if count[property] == 6:
-                return property * 100 * 2 * 2
-
+        # Pairs
+        if len(count) == 3:
+            if all(value == 2 for value in count.values()):
+                return 1500
 
         result = 0
-        if len(tuple) <= 6:
-            counts = Counter(tuple)
-            for number, count in counts.items():
+
+        counts = Counter(tuple)
+        for number, count in counts.items():
+            if count < 3:
                 result += count * DIE_POINTS.get(number, 0)
-                if count == 3:
-                    result += TRIPLET_POINTS[number]
-                    continue
-                if count == 4:
-                    result += TRIPLET_POINTS[number] * 2
-                    continue
-                if count == 5:
-                    result += TRIPLET_POINTS[number] * 3
-                    continue
-                
+            elif count == 3:
+                result += TRIPLET_POINTS[number]
+            elif count == 4:
+                result += TRIPLET_POINTS[number] * 2
+            elif count == 5:
+                result += TRIPLET_POINTS[number] * 3
+            elif count == 6:
+                result += TRIPLET_POINTS[number] * 4
+
         return result
 
     @staticmethod
@@ -60,16 +50,15 @@ class GameLogic:
         INPUT >> Integer - a number between 1 and 6
         OUTPUT >> Tuple - with length of input with numbers between 1 and 6
         """
-        dice_list =  []
+        dice_list = []
         for dice in range(num_dice):
             dice_list.append(random.randint(1, 6))
         return tuple(dice_list)
 
 
-
 if __name__ == "__main__":
 
-    input1 = (1, 1, 1, 1, 1, 1)   
+    input1 = (1, 1, 1, 1, 1, 1)
     # result1 = Counter(input1)
     result = GameLogic.calculate_score(input1)
-    print(result)  
+    print(result)
